c5fe157680d5665cd60889fd838a854a48a4b9a7
Alexis <alexisdavalos.tech@gmail.com>
2020-02-19 15:55:13 -0500

    Set up CRUD router(empty) & created new cars table

---
+++ data/router/cars-router.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const router = express.Router();
+
+//Get Routes
+router.get("/", (req, res) => {
+    
+  });
+  
+router.get("/:id", (req, res) => {
+
+});
+
+//Post Routes
+router.post("/", (req, res) => {
+
+});
+
+//Put Routes
+router.put("/:id", (req, res) => {
+
+});
+
+//Delete Routes
+router.delete("/:id", (req, res) => {
+
+});
+
+
+module.exports = router;
+
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
 // const db = require('./data/dbConfig.js');
-
+const router = require('./data/router/cars-router.js')
 const server = express();
 
 server.use(express.json());
 
-// server.use('/api/accounts', router);
+server.use('/api/cars', router);
 
 server.get('/', (req, res) =>{
     res.send('<h2>Node DB-2 Project</h2>')
